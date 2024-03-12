5c0e6300a793fca39ef703d3f50ffa06b3f602b3
Om Morendha <omkmorendha@gmail.com>
2024-03-12 11:47:33 +0530

    Update restart script for improved logging

--- restart_script.sh
+++ restart_script.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# SSH into the server and restart the service
+# Define the current date and time
+current_datetime=$(date +"%Y-%m-%d_%H-%M-%S")
+
+# SSH into the server, rename the file, and restart the service
 ssh -i "om-key.pem" ubuntu@ec2-54-196-92-178.compute-1.amazonaws.com <<EOF
+    # Rename the log file
+    mv /home/ubuntu/Copy-Trading-API/wrapper.log /home/ubuntu/Copy-Trading-API/wrapper_${current_datetime}.log
+    
+    # Restart the service
     sudo systemctl restart trade
 EOF
