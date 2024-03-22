7e676d5ae4ca3d2fceafa3ade76c80e8a3215e05
Om Morendha <omkmorendha@gmail.com>
2024-03-22 17:14:12 +0530

    Fix CI/CD for dev

--- restart_script.sh
+++ restart_script.sh
@@ -4,7 +4,7 @@
 current_datetime=$(date +"%Y-%m-%d_%H-%M-%S")
 
 # SSH into the server, rename the file, and restart the service
-ssh -i "om-key.pem" ubuntu@ec2-54-196-92-178.compute-1.amazonaws.com <<EOF
+sudo ssh -i "om-key.pem" ubuntu@ec2-34-228-113-56.compute-1.amazonaws.com <<EOF
     # Rename the log file
     mv /home/ubuntu/Copy-Trading-API/wrapper.log /home/ubuntu/Copy-Trading-API/wrapper_${current_datetime}.log
     
